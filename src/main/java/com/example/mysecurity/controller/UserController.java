e7cec450e9b288a4f7d19d793da31ee75e29c7cb
notlikelion <notlikelion@gmail.com>
2025-09-01 15:41:41 +0900

    일단...

---
+++ src/main/java/com/example/mysecurity/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.example.mysecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
